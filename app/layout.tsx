ba2189c2e809250f1352687279f1895ec49e25f1
Dave Jay <davejay3002@gmail.com>
2023-12-15 11:33:52 +0530

    Setup a basic landing page using MaterialUI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,29 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Viewport } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+export const viewport: Viewport = {
+  themeColor: "black",
+  width: "device-width",
+  initialScale: 1,
+};
 
 export const metadata: Metadata = {
-  title: 'Blog App',
-  description: 'BlogApp offers an intuitive writing experience for tech enthusiasts, facilitating seamless content creation',
-}
+  title: "Blog App",
+  description:
+    "BlogApp offers an intuitive writing experience for tech enthusiasts, facilitating seamless content creation",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
 
-module.exports = nextConfig
+module.exports = {
+    webpack: (config, { isServer }) => {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'raw-loader',
+          },
+          {
+            loader: 'markdown-loader',
+            options: {
+              /* your markdown loader options here */
+            },
+          },
+        ],
+      });
+  
+      if (isServer) {
+        config.externals.push({
+          'remark-parse': 'commonjs remark-parse',
+          'remark-stringify': 'commonjs remark-stringify',
+        });
+      }
+  
+      return config;
+    },
+  };
+  
