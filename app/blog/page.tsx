4e3052f25c9266ec7407dc4c347589eca0fd7271
unknown <jatinshrimali24@gmail.com>
2023-12-18 12:41:59 +0530

    Added and Updated the routes components

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -9,19 +9,19 @@ import {
 } from "@mui/material";
 
 interface NewBlogProps {
-  title: string;
-  description: string;
-  image: string;
-  author: string;
-  authorImage: string;
+  title?: string;
+  description?: string;
+  image?: string;
+  author?: string;
+  authorImage?: string;
 }
 
 const NewBlog: React.FC<NewBlogProps> = ({
-  title,
-  description,
-  image,
-  author,
-  authorImage,
+  title = "How Beautiful Our Life Is",
+  description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus consequat aliquet lorem. Fusce semper efficitur augue, at vehicula sem pretium ac. Cras dignissim turpis ipsum, eu laoreet eros pharetra dapibus. Praesent eros turpis, cursus vel sem vel, iaculis lobortis tortor. Integer laoreet erat a leo viverra facilisis. Nullam id cursus tellus, nec fermentum purus. In mattis ac est in vehicula. Ut volutpat, libero quis pretium bibendum, libero nisl tempus lacus, vel consectetur augue nisi sed elit. Mauris elementum vitae diam eget pretium. Etiam sollicitudin metus in lacus malesuada finibus. Pellentesque tincidunt nisi vel enim consectetur, nec venenatis leo feugiat. Cras ut pellentesque mi. Quisque feugiat iaculis justo in vulputate. Nulla facilisi. Sed vitae vehicula purus, quis ornare metus. Quisque vitae dignissim ante, vitae blandit turpis. Nam vel cursus turpis. Suspendisse tempor nisi odio, ornare euismod metus dignissim vel. Vivamus accumsan iaculis nunc ac scelerisque. Ut iaculis consectetur sem, consequat accumsan justo aliquet rhoncus. Curabitur laoreet, arcu quis consectetur semper, mi diam sollicitudin erat, at iaculis augue lorem eget lacus. Maecenas mattis tempus suscipit. Praesent vestibulum tincidunt ligula, ut maximus lorem pharetra in. Fusce ornare dolor risus, a fermentum purus suscipit nec. Vestibulum varius turpis tortor, at cursus dolor dapibus accumsan. Nam porttitor massa nisl, non viverra mi luctus sed. Proin eu augue quis eros convallis luctus. Duis eleifend urna vitae enim aliquam, at vestibulum leo hendrerit. Nulla lacinia nisl vel venenatis feugiat. Morbi gravida semper congue. Suspendisse ac quam nec nunc rutrum tempus. Nam dignissim eros vel ex sollicitudin ornare. Praesent et lacus sapien. Suspendisse mauris ex, rhoncus ac ligula at, pharetra vehicula nisi. Etiam in nisi laoreet, tempus ligula a, hendrerit nunc. Sed eget urna lobortis, sagittis massa nec, finibus erat. Nullam a ligula quis ex faucibus ultricies id et risus. Fusce pellentesque rutrum nulla vel imperdiet. Duis interdum quam at mi mattis consectetur. Curabitur ut suscipit risus. Aenean tempor ullamcorper nisl, sit amet condimentum nunc gravida nec. Donec pretium pulvinar diam ut porta. Nulla lobortis sapien sollicitudin, commodo tellus vel, placerat dui.",
+  image = "https://source.unsplash.com/random?wallpapers",
+  author = "Jatin",
+  authorImage = "https://source.unsplash.com/random?portrait",
 }) => {
   const useStyles = {
     header: {
--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,5 @@
-// UserProfile.tsx
 import React from 'react';
-import { Avatar, Paper, Typography } from '@mui/material';
+import { Avatar, Paper, Typography, Link, Divider } from '@mui/material';
 
 const UserProfile: React.FC = () => {
   const containerStyle: React.CSSProperties = {
@@ -8,11 +7,17 @@ const UserProfile: React.FC = () => {
     flexDirection: 'column',
     alignItems: 'center',
     padding: '16px',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    borderRadius: '16px',
+    boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+    color: '#fff',
+    animation: 'gradientAnimation 10s infinite alternate', // Added animation
   };
 
   const avatarStyle: React.CSSProperties = {
-    width: '80px',
-    height: '80px',
+    width: '100px',
+    height: '100px',
+    marginBottom: '16px',
   };
 
   const usernameStyle: React.CSSProperties = {
@@ -20,16 +25,44 @@ const UserProfile: React.FC = () => {
     fontSize: '1.5rem',
   };
 
+  const detailsStyle: React.CSSProperties = {
+    marginTop: '8px',
+    textAlign: 'center',
+  };
+
+  const dividerStyle: React.CSSProperties = {
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    margin: '12px 0',
+  };
+
   return (
     <Paper elevation={3} style={containerStyle}>
-      <Avatar alt="User Avatar" src="/path/to/avatar.jpg" style={avatarStyle} />
+      <Avatar alt="User Avatar" src="https://source.unsplash.com/random?portrait" style={avatarStyle} />
       <Typography variant="h6" style={usernameStyle}>
         Vikram Dave
       </Typography>
-      <Typography variant="body2">
-        BTech Computer Science Student
-      </Typography>
-      {/* Add more user details or customization as needed */}
+
+      <Divider style={dividerStyle} />
+
+      <div style={detailsStyle}>
+  
+        <Typography variant="body2">
+          Email: vikram.dave@example.com
+        </Typography>
+        <Typography variant="body2">
+          <Link href="https://twitter.com/vikramdave" color="inherit" target="_blank" rel="noopener">
+            Twitter
+          </Link>
+          {' | '}
+          <Link href="https://linkedin.com/in/vikramdave" color="inherit" target="_blank" rel="noopener">
+            LinkedIn
+          </Link>
+          {' | '}
+          <Link href="https://instagram.com/vikramdave" color="inherit" target="_blank" rel="noopener">
+            Instagram
+          </Link>
+        </Typography>
+      </div>
     </Paper>
   );
 };
